aa94f0817d1231408e28214b0fb9a23e013c1bb9
paytonrules <esmith@8thlight.com>
2013-04-25 01:48:05 -0500

    Use the nunit compatible with unity MonoDevelop

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 /Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool -v build "--configuration:Release" CubicleWarsLibrary.csproj
 nunit-console ./bin/Release/CubicleWarsLibrary.dll
-cp bin/Release/CubicleWarsLibrary.dll ../CubicleWarsPrototype/Assets/src
-cp bin/Release/Stateless.dll ../CubicleWarsPrototype/Assets/src
